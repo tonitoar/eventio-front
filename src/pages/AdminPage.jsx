ee27581a73fd34aec15e6b255babfa377a9a2dc4
Toni Torredemer <toni.torredemer.argiles@gmail.com>
2023-06-07 14:59:05 +0200

    estructurar rutes i veure events sol Admin

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,196 +1,70 @@
-import { useState } from "react";
-import { Link, Navigate, useParams } from "react-router-dom";
+import { Link} from "react-router-dom";
+
+
+import AccountNav from "../components/AccountNav.jsx";
+import { useEffect, useState } from "react";
+
 
 export default function AdminPage() {
 
-const {action} = useParams();
-//*console.log(action); 
+    const [events, setEvents] = useState([]);
 
-const [title, setTitle] = useState("");
-const [address, setAddress] = useState("");
-/* const[date, setDate] = useState(new Date()); */
-const [date, setDate] = useState(""); //! o (new Date())
-const [hour, setHour] = useState("");
-const [maxCapacity, setMaxCapacity] = useState(""); 
-const [description, setDescription] = useState(""); 
-const [redirect, setRedirect] = useState(""); 
-//!imagenes
-const [fileInputState] = useState("");
-const [previewSources, setPreviewSources] = useState([]);
-
-
-const handleForSubmit = (e) => {
-    e.preventDefault(); 
-    handleSubmitFile(e);
-    addNewEvent(e);
-};
-
-const handleFileInputChange = (e) => {
-    const files = e.target.files;
-    const filesArray = Array.from(files);
-    filesArray.forEach((file) => {
-        previewFile(file);
-    });
-};
-
-const previewFile = (file) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => {
-        const previewSource = reader.result;
-        setPreviewSources((prevSources) => [...prevSources, previewSource]);
-    };
-};
-
-const handleSubmitFile = (e) => {
-    console.log("submiting");
-    e.preventDefault();
-    if(!previewSources) return;
-    uploadImage(previewSources);
-}
-
-//! Base64 encoding is a way to encode binary data in ASCII text. It's primarily used to store or transfer images, audio files, and other media online. It is also often used when there are limitations on the characters that can be used in a filename for various reasons.
-
-const uploadImage = async (base64EncodedImage) => {
-  // console.log(base64EncodedImage);
-   const token = localStorage.getItem("token");
-    try {
-        await fetch ("http://localhost:3000/api/upload", {
-            method: "POST",
-            body: JSON.stringify({ data: base64EncodedImage, title: title }),
-            headers: {
-                "Content-type": "application/json",
+    useEffect(() => {
+        const fetchEvents = async () => {
+          const token = localStorage.getItem('token');
+          try {
+            const response = await fetch("http://localhost:3000/events", {
+              headers: {
                 Authorization: `Bearer ${token}`,
-            },
-        });
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-
-/* async function addNewEvent (e) {
-    e.preventDefault();
-   await axios.post("http://localhost:3000/events", {
-        title, address, date, 
-        hour, maxCapacity, description,
-        previewSources
-    });
-    setRedirect("/account/events"); 
-} */
-
-async function addNewEvent(e) {
-    e.preventDefault();
-    const token = localStorage.getItem("token");
-    try {
-      const response = await fetch("http://localhost:3000/events", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({
-          title,
-          address,
-          date,
-          hour,
-          maxCapacity,
-          description,
-        }),
-      });
+                "Content-Type": "application/json",
+              },
+            });
+            //console.log(response)
+            if (response.ok) {
+              const data = await response.json();
+             // console.log("DATA", data)
+              setEvents(data);
+            } else {
+              const errorData = await response.json();
+              console.log('Error:', errorData.error);
+              // Handle the error case if needed
+            }
+          } catch (error) {
+            console.log(error);
+            // Handle the error case if needed
+          }
+        };
+      
+        fetchEvents();
+      }, []);
   
-      if (response.ok) {
-        setRedirect("/account/events");
-      } else {
-        console.log("Error:", response.statusText);
-        // Handle the error case if needed
-      }
-    } catch (error) {
-      console.log(error);
-      // Handle the error case if needed
-    }
-  }
-
-if (redirect) {
-    return <Navigate to={redirect} /> 
-}
-
-
-  
-
-//! pujar input multiple
 
     return(
         <div>      
-            {action !== "create" && ( 
+            <AccountNav />
                 <div className="text-center">
                     <Link className="inline-flex gap-1 bg-primary text-black py-2 px-6 rounded-full" to={"/account/admin/create"}> 
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                         </svg>
                         Add new event
-                    </Link>
+                    </Link> 
                 </div>
-            )}
-            {action === "create" && (
-                <div>
-                    <form onSubmit={handleForSubmit}>
-                        <h2 className="text-2xl mt-4">Title</h2>
-                            <p className="text-gray-500 text-sm">Title of the event. Should be short and catchy as in advertisement</p>
-                            <input type="text" 
-                                    placeholder="title, for example: Nevermind" 
-                                    value={title} 
-                                    onChange={(e)=>setTitle(e.target.value)}/>
-                        <h2 className="text-2xl mt-4">Date</h2>
-                            <p className="text-gray-500 text-sm">Date of event</p>
-                            <input type="date" 
-                                    value={date} 
-                                    onChange={(e)=>setDate(e.target.value)}/>
-                                    {/* <DatePicker value={date} onChange={date => setDate(date)}/> */}
-                        <h2 className="text-2xl mt-4">Hour</h2>
-                            <p className="text-gray-500 text-sm">Hour of event</p>
-                            <input type="time" 
-                                    value={hour} 
-                                    onChange={(e)=>setHour(e.target.value)}/> {/* //! ficar AM o PM */}
-                        <h2 className="text-2xl mt-4">Address</h2>
-                            <p className="text-gray-500 text-sm">Addres of the event</p> 
-                            <input type="text" 
-                                    placeholder="address" 
-                                    value={address} 
-                                    onChange={(e)=>setAddress(e.target.value)}/>
-                        <h2 className="text-2xl mt-4">Maximum capacity</h2>
-                            <p className="text-gray-500 text-sm">Maximum capacity of the event</p>
-                            <input type="number" 
-                                    value={maxCapacity} 
-                                    onChange={(e)=>setMaxCapacity(e.target.value)}/>
-                        <h2 className="text-2xl mt-4">Description</h2>
-                            <p className="text-gray-500 text-sm">Description of the event</p>
-                            <textarea  value={description} onChange={(e)=>setDescription(e.target.value)}/>  {/* //! area de text */}
-                        <h2 className="text-2xl mt-4">Photos</h2>
-                            <p className="text-gray-500 text-sm">Photos of the event/location. Max 4. </p>
-                            <div className="flex gap-2 mt-2">
-                                <input type="file" 
-                                        name="image"
-                                        multiple
-                                        value={fileInputState} 
-                                        onChange={handleFileInputChange}/>
+                <div className="mt-4">
+                    {events.length > 0 && events.map ((event, index) => (
+                        <div className="flex cursor-pointer gap-4 bg-gray-100 p-4 rounded-2xl" key={index}>
+                            <div className="w-32 h-32 bg-gray-300 grow shrink-0">
+                                {event.photos.length > 0 && (
+                                    <img src={event.photos[0]} alt="" />
+                                )}
                             </div>
-                            <div className="flex mt-2 gap-2">
-                            {previewSources && previewSources.map((previewSource, index) => (
-                                <img  className="rounded-2xl" key={index} src={previewSource} alt="chosen" style={{ height: "200px" }} />
-                            ))}
-                            </div>
-                        <div>
-                            <button className="cursor-pointer primary my-4 flex gap-1 justify-center p-8 text-2xl" type="submit">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
-                            </svg>
-                            Save
-                            </button>
+                            <div className="grow-0 shrink">
+                            <h2 className="text-xl">{event.title}</h2>
+                            <p className="text-sm mt-2">{event.description}</p>
                         </div>
-                    </form>
+                    </div>
+                    ))}
                 </div>
-            )}
         </div>
     );
 }
---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,68 @@
+import { useContext, useState } from "react";
+import { userContext } from "../contexts/user.context";
+import { Navigate, useParams } from "react-router-dom";
+
+import axios from "axios";
+
+import AdminPage from "./AdminPage";
+import AccountNav from "../components/AccountNav";
+
+export default function ProfilePage () {
+
+    const {user, ready, setUser} = useContext(userContext);
+    //*console.log(user)
+
+    const [redirect, setRedirect] = useState(null); 
+
+
+    let {subpage} = useParams(); //! LET perque canviarem el "undefined" Profile cap a "profile" i menys codi per el LinkClasses
+   //*console.log(subpage);
+
+   
+   if (!ready) {
+       return "Loading ..."; //! NECESARI PER VISUALTZAR EL USERNAME
+    }
+    
+    if (ready && !user && !redirect) { //! !redirect per comprobar que no tenim cap altre redirecció per fer logout --> indexpage
+        return <Navigate to={"/login"} /> //! NECESARI PER VISUALTZAR EL USERNAME
+    }
+    
+
+    if (subpage === undefined) {
+     subpage = "profile"; 
+    }
+
+
+
+//! type=null (0 diseny), per definicio nul. Donem classe predefinida (py + px).Si el type es igual a subpage (***), en la classe predefinida li afegim uns valors de diseny amb " ESPAI"
+//TODO *** => profile canvia perque es "undefind", per lo tant hem de afegir (...|| (subpage === undefined && type === "profile"))
+
+
+
+async function logout() {
+    localStorage.removeItem("token");
+    await axios.post("/logout")
+    setRedirect("/");
+    setUser(null); //! primer redirect, despres borrar dades usuari
+ }
+
+
+if (redirect) {
+    return <Navigate to={redirect} />
+}
+
+    return (    
+        <div>
+            <AccountNav />
+            {subpage === "profile" && (
+                <div className="text-center max-w-lg mx-auto">
+                    Logged in as {user.username} ({user.email}) <br />
+                    <button onClick={logout} className="primary max-w-sm mt-2">Logout</button>
+                </div>
+            )}
+            {subpage === "admin" && (
+                <AdminPage />
+            )}
+        </div> 
+    );
+} 
